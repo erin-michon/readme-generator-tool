f69777d1cbc14abe9a26e672610344e4de1d0ec5
Erin M <erin.michon@gmail.com>
2021-12-04 14:19:50 -0500

    confirmed all general prompts

--- index.js
+++ index.js
@@ -1,8 +1,43 @@
 // TODO: Include packages needed for this application
-let inquirer = require('inquirer');
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: "What is the title of your project?"
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: "Please provide a description of your project."
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: "Please provide the required installation steps for your project."
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: "Please provide instructions and examples for the use of your project."
+        },
+        {
+            type: 'input',
+            name: 'collab',
+            message: "Please provide contribution guidelines you have for your project."
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: "Please provide a test and an example of how to run them on your project."
+        }
+
+    ]);
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -12,3 +47,5 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+questions();
